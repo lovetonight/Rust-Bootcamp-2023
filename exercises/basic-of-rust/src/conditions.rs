270e53ac5a6f2999ed8fea3cf397e09bffcf9232
dthoanhust204974 <hoan.dt204974@sis.hust.edu.vn>
2023-07-10 11:15:25 +0700

    done conditions

--- exercises/basic-of-rust/src/conditions.rs
+++ exercises/basic-of-rust/src/conditions.rs
@@ -4,15 +4,24 @@
 // - another function call
 // - additional variables
 pub fn bigger(a: i32, b: i32) -> i32 {
-    todo!()
+    if a>b{
+        return a;
+    }
+    else {return b;}
 }
 
 //Exercise 2
 // Input: Provide an arbitrary value of number
 // Check number is Positive or Negative or Zero
 // Output: &str
-fn check_number(number: u32) -> &'static str {
-    todo!()
+fn check_number(number: i32) -> &'static str {
+    if number > 0 {
+        "Positive"
+    } else if number < 0 {
+        "Negative"
+    } else {
+        "Zero"
+    }
 }
 
 // Exercise 3
@@ -22,33 +31,62 @@ fn check_number(number: u32) -> &'static str {
 pub fn foo_if_fizz(fizzish: &str) -> &str {
     if fizzish == "fizz" {
         "foo"
-    } else {
-        1
+    } else if fizzish == "fuzz" {
+        "bar"
     }
+    else 
+    {"baz"}
 }
 
 // Exercise 4
 // Determine if a given year is a leap year
 // Implement logic
 fn is_leap_year(year: i32) -> bool {
-    todo!()
+    if year % 100 == 0 {
+        if year % 400 == 0 {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    if year % 4 == 0 {
+        return true;
+    }
+    else 
+    {return false;}
 }
 
 // Exercise 5
 // Calculate the factorial of a number
 // Implement logic
 fn factorial(n: u32) -> u32 {
-    todo!()
+    if n == 0 {
+        1
+    } else {
+        n * factorial(n - 1)
+    }
 }
+
 
 // Exercise 6
 // Check if a number is prime
 // Implement logic
 
 fn is_prime(n: u32) -> bool {
-    todo!()
-}
+    if n <= 1 {
+        return false;
+    }
 
+    let mut i = 2;
+    while i * i <= n {
+        if n % i == 0 {
+            return false;
+        }
+        i += 1;
+    }
+
+    true
+}
 
 // Don't mind this for now :)
 #[cfg(test)]
@@ -143,6 +181,4 @@ mod tests {
         assert_eq!(is_prime(10), false);
         assert_eq!(is_prime(15), false);
     }
-
-
 }
